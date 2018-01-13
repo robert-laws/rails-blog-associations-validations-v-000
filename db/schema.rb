4a64602a864918fdf81297f763367fddef4d3cd0
Robert Laws <roblaws@me.com>
2018-01-13 11:48:09 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180113114131) do
+ActiveRecord::Schema.define(version: 20180113114737) do
 
   create_table "post_tags", force: :cascade do |t|
     t.integer  "tag_id"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180113114131) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "content"
+    t.integer  "user_id"
   end
 
   create_table "tags", force: :cascade do |t|
